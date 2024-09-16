9e39fc2a4ff54f64f3ed47e5bf417fa606cd775b
Colocho10 <carlosp27069@gmail.com>
2024-09-15 23:45:21 -0600

    Redis y DTOs

--- UsuariosAPI/Models/Permiso.cs
+++ UsuariosAPI/Models/Permiso.cs
@@ -6,15 +6,8 @@ namespace UsuariosAPI.Models
     public class Permiso
     {
         [Key]
-        public int Id { get; set; }
-
-        [Required]
-        [StringLength(50, MinimumLength = 3)]
+        public int Id { get; set; } // Este campo se generará automáticamente
         public string Nombre { get; set; }
-
         public string Descripcion { get; set; }
-
-        // Relación uno a muchos con Roles
-        public ICollection<Rol> Roles { get; set; }
     }
 }
